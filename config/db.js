ec60143d787af3c3cc92c9f40f84e29b61936744
Tuan1601 <NATTuan1601@gmail.com>
2024-12-24 20:27:04 +0700

    Tuan1601

--- config/db.js
+++ config/db.js
@@ -2,12 +2,12 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'documentsharing',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10,
+  connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT, 10),
   queueLimit: 0,
 });
 
